a5c853b32ca369ba5246b45e6de6f53960662548
Pranav104-1 <pranavnere647@gmail.com>
2026-02-23 13:03:24 +0530

    frontend

---
+++ frontend/script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("registrationForm");
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", async function (e) {
+e.preventDefault();
+
+const data = {
+name: document.getElementById("name").value,
+email: document.getElementById("email").value,
+phone: document.getElementById("phone").value,
+college: document.getElementById("college").value,
+year: document.getElementById("year").value,
+branch: document.getElementById("branch").value,
+event: document.getElementById("event").value
+};
+
+const registerButton = document.getElementById("registerbutton");
+registerButton.disabled = true;
+registerButton.innerText = "Registering...";
+registerButton.onclick = console.log(data);
+try {
+
+const res = await fetch("http://localhost:5000/register", {
+method: "POST",
+headers: { "Content-Type": "application/json" },
+body: JSON.stringify(data)
+});
+
+const result = await res.json();
+
+successMessage.innerText = result.message;
+form.reset();
+
+} catch (error) {
+
+successMessage.innerText = "❌ Server Error!";
+
+}
+
+});
